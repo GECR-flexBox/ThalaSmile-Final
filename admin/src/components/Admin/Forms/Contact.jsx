46dd67d0c81d098c13c87f7622908cd699d860e2
Devrajsinh Gohil <devcode03@gmail.com>
2023-04-27 16:14:27 +0530

    useState in constact.jsx

--- admin/src/components/Admin/Forms/Contact.jsx
+++ admin/src/components/Admin/Forms/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../Css/Contact.css";
 import "../../../Css/EventForm.css";
 import FestivalIcon from "@mui/icons-material/Festival";
@@ -6,7 +6,15 @@ import FestivalOutlinedIcon from "@mui/icons-material/FestivalOutlined";
 import AddLocationAltRoundedIcon from "@mui/icons-material/AddLocationAltRounded";
 import DescriptionRoundedIcon from "@mui/icons-material/DescriptionRounded";
 import DateRangeRoundedIcon from "@mui/icons-material/DateRangeRounded";
+
 const Contact = () => {
+
+  const [eventName, setEventName] = useState("")
+  const [date, setDate] = useState("")
+  const [evenPoster, setEvenPoster] = useState("")
+  const [location, setLocation] = useState("")
+  const [eventDesc, setEventDesc] = useState("")
+
   return (
     <div>
       <header className="event-sec">
@@ -22,13 +30,17 @@ const Contact = () => {
             <label className="icon">
               <FestivalOutlinedIcon />
             </label>
-            <input type="text" className="name" placeholder="Event Name" />
+            <input type="text" className="name" placeholder="Event Name" onChange={(e) => {
+                setEventName(e.target.value)
+              }}/>
           </div>
           <div className="input">
             <label className="icon">
               <DateRangeRoundedIcon />
             </label>
-            <input type="date" className="name" placeholder="" />
+            <input type="date" className="name" placeholder="" onChange={(e) => {
+                setDate(e.target.value)
+              }}/>
           </div>
           <div className="input">
             <label className="icon">
@@ -36,14 +48,18 @@ const Contact = () => {
             </label>
             <div className="nn2">
               <div className="tx">Upload Event Poster </div>
-              <input type="file" name="" id="" />
+              <input type="file" name="" id="" onChange={(e) => {
+                setEvenPoster(e.target.value)
+              }}/>
             </div>
           </div>
           <div className="input">
             <label className="icon">
               <AddLocationAltRoundedIcon />
             </label>
-            <input type="text" className="name" placeholder="Event Location" />
+            <input type="text" className="name" placeholder="Event Location" onChange={(e) => {
+                setLocation(e.target.value)
+              }}/>
           </div>
           <div className="inmsg">
             <label className="icon msicon">
@@ -56,10 +72,19 @@ const Contact = () => {
               rows="10"
               className="msg"
               placeholder="Event Description"
+              onChange={(e) => {
+                setEventDesc(e.target.value)
+              }}
             />
           </div>
           <div className="forbtn">
-            <div className="btn">Add Event</div>
+            <div className="btn" onClick={() => {
+              console.log({eventName});
+              console.log({date});
+              console.log({evenPoster});
+              console.log({location});
+              console.log({eventDesc});
+            }}>Add Event</div>
           </div>
         </form>
       </div>
